a8fd2027ccdc363789b20a2635344502b3724fc6
Lucas Faleiro <lucas.faleirosilva@gmail.com>
2026-03-06 16:58:16 -0300

    Finish project, kinda forgot to commit before, was in the zone

--- script.js
+++ script.js
@@ -1,27 +1,105 @@
 const bookList = document.querySelector("#book-list");
 
-const myLibrary = [
-  {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    pages: 295,
-    read: false,
-  },
-];
+const myLibrary = [];
 
-for (let book of myLibrary) {
-  const bookContainer = document.createElement("div");
-  bookList.appendChild(bookContainer);
+console.log(Object.entries(myLibrary));
 
-  for (let propertyValue of Object.values(book)) {
-    console.log(propertyValue);
+function displayBooks() {
+  bookList.innerHTML = "";
+  for (let book of myLibrary) {
+    const bookContainer = document.createElement("div");
+    bookList.appendChild(bookContainer);
+    bookContainer.setAttribute("class", "book-card");
 
-    const span = document.createElement("span");
-    span.innerText = propertyValue;
-    bookContainer.appendChild(span);
+    for (let [key, value] of Object.entries(book)) {
+      if (key === "id") continue;
+
+      if (key === "read") {
+        const readBtn = document.createElement("button");
+
+        if (value === true) {
+          readBtn.innerText += "Already Read";
+          readBtn.setAttribute("id", "already-read");
+        } else {
+          readBtn.innerText += "Not Read";
+          readBtn.setAttribute("id", "not-read");
+        }
+        readBtn.addEventListener("click", () => {
+          value = !value;
+          if (value === true) {
+            readBtn.innerText = "Already Read";
+            readBtn.setAttribute("id", "already-read");
+          } else {
+            readBtn.innerText = "Not Read";
+            readBtn.setAttribute("id", "not-read");
+          }
+        });
+        bookContainer.appendChild(readBtn);
+        continue;
+      }
+
+      const span = document.createElement("span");
+
+      if (key === "pages") {
+        span.innerText += "Pages: ";
+      }
+
+      span.innerText += value;
+      bookContainer.appendChild(span);
+      span.setAttribute("class", "book-item");
+    }
+    const removeBookBtn = document.createElement("button");
+    removeBookBtn.innerText = "Remove Book";
+    removeBookBtn.setAttribute("id", "remove-book-btn");
+    bookContainer.appendChild(removeBookBtn);
+    removeBookBtn.addEventListener("click", () => {
+      const bookIndex = myLibrary.indexOf(book);
+      myLibrary.splice(bookIndex, 1);
+      displayBooks();
+    });
   }
 }
 
-function Book() {}
+const openFormBtn = document.querySelector("#open-form-btn");
+const addBookBtn = document.querySelector("#add-book-btn");
+const dialogForm = document.querySelector("#modal");
 
-function addBookToLibrary() {}
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
+const bookRead = document.querySelector("#read");
+
+openFormBtn.addEventListener("click", () => {
+  dialogForm.showModal();
+});
+
+addBookBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const title = bookTitle.value;
+  const author = bookAuthor.value;
+  const pages = bookPages.value;
+  const read = bookRead.checked;
+  addBookToLibrary(title, author, pages, read);
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookPages.value = "";
+  bookRead.value = "";
+  dialogForm.close();
+});
+
+function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  displayBooks();
+  console.log(myLibrary);
+}
+
+displayBooks();
